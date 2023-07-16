ea6723c82b1b19461557d900675df862223ff532
Jared Levi De La Fuente Rodriguez <jk20042019@gmail.com>
2023-07-15 21:51:12 -0600

    Update voice.py

--- mark_2/features/voice.py
+++ mark_2/features/voice.py
@@ -1,1 +1,19 @@
-ju = 4
+import os
+import pygame
+import pyttsx3
+
+engine = pyttsx3.init("sapi5")
+voices = engine.getProperty('voices')
+female_voice = None
+for voice in voices:
+    if 'female' in voice.name.lower():
+        female_voice = voice
+        break
+if female_voice is not None:
+    engine.setProperty('voice', female_voice.id)
+else: print("Female voice not found.")
+
+# function to speak
+def speak(text):
+    engine.say(text=text)
+    engine.runAndWait()
