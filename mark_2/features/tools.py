dc335a85fdc2750fb1c45aa94c7cf47fc269e780
Jared Levi De La Fuente Rodriguez <jk20042019@gmail.com>
2023-07-21 21:14:25 -0600

    Update tools.py

--- mark_2/features/tools.py
+++ mark_2/features/tools.py
@@ -1,35 +1,44 @@
-from langchain import OpenAI
-import os
+# Import things that are needed generically
+from langchain import LLMMathChain, SerpAPIWrapper
+from langchain.agents import AgentType, initialize_agent
 from langchain.chat_models import ChatOpenAI
-from langchain.chains.conversation.memory import ConversationBufferWindowMemory
-from langchain.agents import Tool
-from langchain.agents import initialize_agent
-from langchain.tools import BaseTool
+from langchain.tools import BaseTool, StructuredTool, Tool, tool
 from langchain.tools import DuckDuckGoSearchRun
+from pydantic import BaseModel, Field
+from langchain.utilities import WikipediaAPIWrapper, PythonREPL
+import os
 
-os.environ["OPENAI_API_KEY"] = ""
-# setup the turbo LLM
-turbo_llm = ChatOpenAI(
-    temperature=0,
-    model="gpt-3.5-turbo"
-)
+os.environ["OPENAI_API_KEY"] = "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"
 
+# initializing the llm
+llm = ChatOpenAI(temperature=0.7)
 search = DuckDuckGoSearchRun()
+wikipedia = WikipediaAPIWrapper()
+python_repl = PythonREPL()
+
 tools = [
-    Tool(
-        name = "search",
-        func = search.run,
-        description="useful for when you need something from the internet",
+    Tool.from_function(
+        func=search.run,
+        name='Search',
+        description="Useful for when you need to answer questions about current events",
+    ),
+
+    Tool.from_function(
+        func=wikipedia.run,
+        name='Wikipedia',
+        description='Useful for when you need to look up a topic, country or person on wikipedia'
+    ),
+
+    Tool.from_function(
+        func=python_repl.run,
+        name='PythonREPL',
+        description='Useful for when you need to use python to answer a question, You shoud input python code.'
     )
 ]
 
-# creating an agent
-memory = ConversationBufferWindowMemory(
-    memory_key='chat-history',
-    k=3,
+# make the agent
+agent = initialize_agent(
+    tools=tools, llm=llm, agent=AgentType.ZERO_SHOT_REACT_DESCRIPTION, verbose=True
+)    k=3,
     return_messages=True
 )
-
-conversational_agent = initialize_agent(tools=tools, llm=turbo_llm, agent=memory)
-
-conversational_agent("What time is it in London?")
