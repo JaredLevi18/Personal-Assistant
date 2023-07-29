b6ea5c02c99f4bd085f407426ccacc6ae40928c9
Jared Levi De La Fuente Rodriguez <jk20042019@gmail.com>
2023-07-28 18:43:47 -0600

    Update listen.py

--- mark_2/features/listen.py
+++ mark_2/features/listen.py
@@ -3,18 +3,18 @@ import speech_recognition as sr
 def listen():
     r = sr.Recognizer()
     with sr.Microphone() as source:
-        print("Listening")
+        print("Say something!")
         r.adjust_for_ambient_noise(source=source, duration=1)
         audio = r.listen(source)
 
+    # recognize speech using google
     try:
-        print("Recognizing")
         command = r.recognize_google(audio)
-        print("You said" + command)
-        return command.lower()
+        print("It thinks you said " + command)
+        return command
     except sr.UnknownValueError:
-        print("Sorry, I couldn't understand. Please try againg.")
-        return ""
-    except sr.RequestError:
-        print("Sorry, I'm not able to acces the speech recognition service.")
-        return ""
+        print("It could not understand audio")
+        return ''
+    except sr.RequestError as e:
+        print("It error; {0}".format(e))        
+        return ''
